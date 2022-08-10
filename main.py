42b637b1f71b320e637365f96f8666d9899f80f0
Reese <86006893+Spyabo@users.noreply.github.com>
2022-08-10 13:20:34 +0100

    made haversine formula more accurate

--- main.py
+++ main.py
@@ -34,12 +34,13 @@ def haversine(lon1, lat1, lon2, lat2):
     # convert decimal degrees to radians
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
+    r = 6372.8  # Radius of earth in kilometers. Use 3959.87433 for miles.
+    
     # haversine formula
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
-    c = 2 * asin(sqrt(a))
-    r = 6371  # Radius of earth in kilometers. Use 3956 for miles.
+    c = 2 * asin(sqrt(a)
     return c * r
 
 
